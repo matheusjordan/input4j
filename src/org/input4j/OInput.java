0ea2b95c573c2c85a668251a5b82c4f36c42a109
matheusjordan <matheusj1599@outlook.com>
2019-05-04 00:46:14 -0300

    Created OInput and fixed methods name.

--- src/org/input4j/OInput.java
+++ src/org/input4j/OInput.java
@@ -2,29 +2,29 @@ package org.input4j;
 
 import java.util.Scanner;
 
-public abstract class Input {
+public class OInput {
 	private static Scanner read = new Scanner(System.in);
-
-	public static int Int(String menssage) {
+	
+	public int Int(String menssage) {
 		System.out.printf(menssage);
 		Integer i = Integer.parseInt(read.next());
 		return i;
 	}
 
-	public static long Lng(String menssage) {
+	public long Lng(String menssage) {
 		System.out.printf(menssage);
 		Long l = Long.parseLong(read.next());
 		return l;
 	}
 
-	public static char Chr(String menssage) {
+	public char Chr(String menssage) {
 		System.out.printf(menssage);
 		Character c = read.next().charAt(0);
 		return c;
 	}
 
 	//Impossivel utilizar o mesmo objeto dos outros inputs
-	public static String Str(String menssage) {
+	public String Str(String menssage) {
 		Scanner read = new Scanner(System.in);
 		System.out.printf(menssage);
 		String s = read.next();
@@ -36,19 +36,19 @@ public abstract class Input {
 		return s;
 	}
 
-	public static float Flt(String menssage) {
+	public float Flt(String menssage) {
 		System.out.printf(menssage);
 		Float v = Float.parseFloat(read.next());
 		return v;
 	}
 
-	public static double Dob(String menssage) {
+	public double Dob(String menssage) {
 		System.out.printf(menssage);
 		Double d = Double.parseDouble(read.next());
 		return d;
 	}
 
-	public static boolean Bool(String menssage) {
+	public boolean Bool(String menssage) {
 		System.out.printf(menssage);
 		Boolean b = Boolean.parseBoolean(read.next());
 		return b;
